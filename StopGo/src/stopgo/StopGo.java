747e67e485d0142da056a7530d16c21c738a8629
ST10508330 <ST10508330@MYEMERIS.EDU.ZA>
2026-02-25 08:15:55 +0200

    Stop&Go

---
+++ StopGo/src/stopgo/StopGo.java
@@ -0,0 +1,44 @@
+/**
+ * My Stop & Go Project
+ */
+
+package stopgo;
+
+/**
+ *
+ * @author ST10508330
+ */
+public class StopGo 
+{
+
+   
+    
+    public static void main(String[] args) 
+    {
+       
+        // Stop & Go sign outprint
+       
+        System.out.println("|-----------------------------------------|");
+        System.out.println("|              |-----------|              |");
+        System.out.println("|              |    STOP   |              |");
+        System.out.println("|              |     &     |             |");
+        System.out.println("|              |    GO     |              |");
+        System.out.println("|              |-----------|              |");
+        System.out.println("|     ********                 XXXXXXXX   |");
+        System.out.println("|    *        *               X        X  |");
+        System.out.println("|   *   STOP   *             X    GO    X |");
+        System.out.println("|    *        *               X        X  |");
+        System.out.println("|     ********                 XXXXXXXX   |");
+        System.out.println("|        **                       XX      |");
+        System.out.println("|        **                       XX      |");
+        System.out.println("|        **                       XX      |");
+        System.out.println("|        **                       XX      |");
+        System.out.println("|       ****                     XXXX     |");
+        System.out.println("|    **********               XXXXXXXXXX  |");
+        System.out.println("|-----------------------------------------|");
+        System.out.println("|-----------------------------------------|");
+        
+
+    }
+    
+}
